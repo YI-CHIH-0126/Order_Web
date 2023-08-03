25af4007ee5441eb248bd09524fe32a977d829e5
YI-CHIH-0126 <kingwang.tw2014@gmail.com>
2023-08-03 23:55:53 +0800

    finish check order

--- main.py
+++ main.py
@@ -9,10 +9,35 @@ def index():
 def order():
     return render_template("order.html")
 
-@app.route("/check_order")
+@app.route("/check_order",methods=['POST','GET'])
 def check():
-    return render_template("check.html")
-
+    if request.method =='POST':
+        file=open("static/list.txt",mode='a')
+        print("{}:{}".format(request.form['phone'],request.form['item']),file=file)
+        file.close()
+        file=open('static/list.txt',mode='r')
+        olist=file.readlines()
+        html='''
+                <h1>order list</h1>
+                <table border>
+            '''
+        for i in olist:
+            html+='<tr><td>{}</td></tr>'.format(i)
+        html+='</table>'
+        file.close()
+        return html
+    else:
+        html='''
+                <h1>order list</h1>
+                <table border>
+            '''
+        file=open("static/list.txt","r")
+        olist=file.readlines()
+        for i in olist:
+            html+="<tr><td>{}</td></tr>".format(i)
+        html+='</table>'
+        file.close()
+        return html
 @app.route("/log_in")
 def log_in():
     return render_template("login.html")
